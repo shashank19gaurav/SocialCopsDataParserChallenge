c2f480bb10df52cd9a72a6355eedcc4655eee445
Shashank Gaurav <shashank19gaurav@gmail.com>
2016-04-05 22:26:51 +0530

    Extract EPIC No Parse Website to get Data and generate CSV

--- parsedemo.py
+++ parsedemo.py
@@ -1,6 +1,9 @@
-import requests, re, json
+import requests, re, json, re
 import unicodecsv as csv
 from bs4 import BeautifulSoup
+
+iterator = 1
+headerList = ['S No.', 'AC No', 'Part No', 'Section No', 'Serial No', 'First Name', 'Name (Hindi)', 'Father/ Husband\'s Name', 'Father/ Husband\'s Name (Hindi)', 'Age', 'Gender', 'EPIC No']
 
 def extractHiddenFields(formParmas, soup):
     for inputType in soup.find_all('input'):
@@ -12,59 +15,63 @@ def setDefaultFormFields(formParams):
     formParams['__EVENTARGUMENT'] = ''
     formParams['__EVENTTARGET'] = 'ddlDistricts'
     formParams['__LASTFOCUS'] = ''
-
     # District Code of Faizabad
     formParams['ddlDistricts'] = 67
     return formParams
 
-# Get the inital page
-response = requests.get("http://164.100.180.4/searchengine/SearchEngineEnglish.aspx")
-soup = BeautifulSoup(response.text, 'lxml')
+def parseData(epicNo):
+    global iterator, formParams, headerList
+    response = requests.get("http://164.100.180.4/searchengine/SearchEngineEnglish.aspx")
+    soup = BeautifulSoup(response.text, 'lxml')
 
-# Prepare for inital Post Request to get the form which will contain the input field to enter EPIC No
-formParams = {}
-formParams = extractHiddenFields(formParams, soup)
-formParams = setDefaultFormFields(formParams)
+    # Prepare for inital Post Request to get the form which will contain the input field to enter EPIC No
+    formParams = {}
+    formParams = extractHiddenFields(formParams, soup)
+    formParams = setDefaultFormFields(formParams)
 
-# Get the page to enter EPIC No
-selectedDistrictForm = requests.post('http://164.100.180.4/searchengine/SearchEngineEnglish.aspx', data = formParams)
-selectedDistrictSoup = BeautifulSoup(selectedDistrictForm.text, 'lxml')
+    # Get the page to enter EPIC No
+    selectedDistrictForm = requests.post('http://164.100.180.4/searchengine/SearchEngineEnglish.aspx', data = formParams)
+    selectedDistrictSoup = BeautifulSoup(selectedDistrictForm.text, 'lxml')
 
-# Check if previous request was successful or not
-if len(selectedDistrictSoup.body.findAll(text=re.compile('EPIC'))) > 0:
-    # Yo, first request was a success
-    pass
-else:
-    # error quit the program
-    pass
+    # Prepare for final request to get the required information
+    finalFormParams = {}
+    finalFormParams = extractHiddenFields(finalFormParams, selectedDistrictSoup)
+    finalFormParams =  setDefaultFormFields(finalFormParams)
+    finalFormParams['txtEPICNo'] = epicNo
+    finalFormParams['RdlSearchBy'] = 0
+    getVoterDetails = requests.post('http://164.100.180.4/searchengine/SearchEngineEnglish.aspx', data = finalFormParams)
+    finalDetailsSoup = BeautifulSoup(getVoterDetails.text, 'lxml')
 
-# Prepare for final request to get the required information
-finalFormParams = {}
-finalFormParams = extractHiddenFields(finalFormParams, selectedDistrictSoup)
-finalFormParams =  setDefaultFormFields(finalFormParams)
-finalFormParams['txtEPICNo'] = 'ANB1882687'
-finalFormParams['RdlSearchBy'] = 0
-getVoterDetails = requests.post('http://164.100.180.4/searchengine/SearchEngineEnglish.aspx', data = finalFormParams)
-finalDetailsSoup = BeautifulSoup(getVoterDetails.text, 'lxml')
+    with open('data.csv', 'a') as csvWriterFile:
+        csvWriter = csv.writer(csvWriterFile)
 
-dateTable = finalDetailsSoup.findChildren('table', {'id': 'gvSearchResult'})[0]
-dataRow = dateTable.findChildren(['tr'])[1]
-dataCell = dataRow.findChildren('td')
+        if(len(finalDetailsSoup.findChildren('table', {'id': 'gvSearchResult'}))) > 0:
+            dateTable = finalDetailsSoup.findChildren('table', {'id': 'gvSearchResult'})[0]
+            dataRow = dateTable.findChildren(['tr'])[1]
+            dataCell = dataRow.findChildren('td')
 
-headerList = ['AC No', 'Part No', 'Section No', 'Serial No', 'First Name', 'Name (Hindi)', 'Father/ Husband\'s Name', 'Father/ Husband\'s Name (Hindi)', 'Age', 'Gender', 'EPIC No']
+            # Writer headers
+            if iterator == 1:
+                csvWriter.writerow(headerList)
+            dataList = []
+            dataCell.pop(0)
+            for cell in dataCell:
+                value = "" + cell.string 
+                dataList.append(cell.string)
+            dataList.insert(0, iterator)
+            print dataList
+            csvWriter.writerow(dataList)
+        else:
+                 notFound   = [iterator]
+                 notFound += ["Not Found"] * 10
+                 notFound.append(epicNo)
+                 csvWriter.writerow(notFound)
 
-with open('data.csv', 'w') as csvWriterFile:
-    csvWriter = csv.writer(csvWriterFile)
-    # Writer headers
-    csvWriter.writerow(headerList)
-
-    dataList = []
-    dataCell.pop(0)
-    for cell in dataCell:
-        value = "" + cell.string 
-        dataList.append(cell.string)
-        print "The value in this cell is %s" % value
-    print dataList
-    csvWriter.writerow(dataList)
-
-# Save it to csv
+with open("epicNoList.txt", "r") as filestream:
+    for line in filestream:
+        epicNoList = line.split(",")
+for epicNo in epicNoList:
+    print "Finding data for Epic No", epicNo
+    if iterator>9:
+        parseData(epicNo)
+    iterator = iterator + 1
--- tryextract.py
+++ tryextract.py
@@ -1,2 +1,18 @@
-import textract
-text = textract.process("S24A276P001.pdf")
+import re
+epicNoList=[]
+with open('output.text', 'r') as myfile:
+    data=myfile.read()
+    epicNoList = re.findall(r'ANB[0-9]{7}', data)
+    epicNoList = epicNoList + re.findall(r'GLT[0-9]{7}', data)
+    epicNoList = epicNoList + re.findall(r'UP/[0-9]{2}/[0-9]{3}/[0-9]{7}', data)
+    
+
+for i in epicNoList:
+    print i
+
+print "Size of the list :", len(epicNoList)
+print "Writing output to a file"
+
+mylist = epicNoList
+with open('epicNoList.txt','w') as epicNoListfile:
+    epicNoListfile.write( ','.join( mylist ) )
